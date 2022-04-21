6cda0bb9de83f101203cc3bdeb7ed945e8ad9b05
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2022-04-21 09:57:10 +0100

    gitpkg

---
+++ dist/entries/WrappedEntryCard/WrappedEntryCard.d.ts
@@ -0,0 +1,31 @@
+/// <reference types="react" />
+import { SpaceAPI } from '@contentful/app-sdk';
+import { ContentType, Entry, RenderDragFn } from '../../types';
+export interface WrappedEntryCardProps {
+    getEntityScheduledActions: SpaceAPI['getEntityScheduledActions'];
+    getAsset: (assetId: string) => Promise<unknown>;
+    entryUrl?: string;
+    size: 'small' | 'default' | 'auto';
+    isDisabled: boolean;
+    isSelected?: boolean;
+    onRemove?: () => void;
+    onEdit?: () => void;
+    localeCode: string;
+    defaultLocaleCode: string;
+    contentType?: ContentType;
+    entry: Entry;
+    renderDragHandle?: RenderDragFn;
+    isClickable?: boolean;
+    hasCardEditActions: boolean;
+    onMoveTop?: () => void;
+    onMoveBottom?: () => void;
+    hasMoveOptions?: boolean;
+}
+export declare function WrappedEntryCard(props: WrappedEntryCardProps): JSX.Element;
+export declare namespace WrappedEntryCard {
+    var defaultProps: {
+        isClickable: boolean;
+        hasCardEditActions: boolean;
+        hasMoveOptions: boolean;
+    };
+}
